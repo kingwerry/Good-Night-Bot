91542d81ca676c0eff11296135578e13e127e6fc
Vir <mrvir@protonmail.com>
2017-09-16 16:45:31 +0700

    first commit

---
+++ config.py
@@ -0,0 +1,14 @@
+﻿friendlist = {'100009733325500': 'Beo Tó','100003725966932': 'An Bùi Phú'}
+
+wish = ['❤Bé {0} ơi ngủ đi❤ \nĐêm đã khuya rồi 😪 \nĐể những giấc mơ đẹp. Sẽ luôn bên em 😪', 
+        '❤Chúc ngủ ngon {0} \nNhiều ác mộng!!!❤ \nNgủ đi thôi bạn ê 😪 \nThức khuya ko tốt đâu ☺☺']
+
+images = ['http://www.desicomments.com/dc3/14/407456/407456.jpg',
+'https://i.pinimg.com/236x/e2/b9/c9/e2b9c9ccf09d524c924928ae96eba4e5--good-night-friends-gn.jpg',
+'https://i.pinimg.com/236x/f8/c4/07/f8c40730e0440be0827ecc18ab843a82.jpg',
+'https://i.pinimg.com/236x/26/d1/1b/26d11ba92801dd21ff968221a9c9155a.jpg',
+'https://i.pinimg.com/236x/87/20/49/8720499c3337d82367a483729788e1e0--good-night-quotes-morning-images.jpg',
+'https://i.pinimg.com/236x/bb/44/9f/bb449f8366efa5d889d10c7488c8d41e--good-night-sweet-dreams-goodnight-and-sweet-dreams.jpg']
+
+username = ''
+password = ''
